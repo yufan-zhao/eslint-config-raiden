9bd3e796fab22d6b343e96baca29b3889c0018ce
Yufan <yufan.zhao@hotmail.com>
2021-12-28 18:10:07 +0800

    初始化仓库

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * @see https://eslint.org/docs/developer-guide/shareable-configs
+ */
+module.exports =
+{
+    parserOptions:
+    {
+		"ecmaVersion": 2021,
+        "ecmaFeatures":
+        {
+            "jsx": true
+        },
+        "sourceType": "module"
+	},
+    
+    env:
+    {
+        "node": true
+    },
+
+    /**
+     * @see https://eslint.org/docs/rules/
+     */
+    rules:
+    {
+        // Possible Problems
+        "no-unused-vars": [1,
+			{
+				"vars": "local",
+				"args": "none",
+				"ignoreRestSiblings": false,
+				"argsIgnorePattern": "^_",
+				"caughtErrors": "none"
+			}
+		],
+		// Suggestions
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"quotes": [2, "double", {"allowTemplateLiterals": true}],
+		// Layout & Formatting
+		"indent": [2, "tab", {
+			"SwitchCase": 4
+		}],
+		"brace-style": [2, "allman", {
+			"allowSingleLine": true
+		}],
+		"semi": [2, "always"]
+    }
+};
